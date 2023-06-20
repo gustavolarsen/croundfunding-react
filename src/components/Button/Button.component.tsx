727f9b93554732ee1a505c7437efbe3dfb7e4a1f
Gustavo Larsen <guslarsen@gmail.com>
2023-06-19 22:55:01 -0300

    feat: disable and click button function

--- src/components/Button/Button.component.tsx
+++ src/components/Button/Button.component.tsx
@@ -4,9 +4,16 @@ interface Props {
   title: string;
   disabled?: boolean;
 }
+
 const Button = ({ title, disabled }: Props) => {
   return (
-    <button className={`${styles.primary} ${disabled && styles.disabled} `}>
+    <button
+      className={`${styles.primary} ${disabled && styles.disabled}`}
+      disabled={disabled}
+      onClick={() => {
+        alert('click button');
+      }}
+    >
       {disabled ? `Out of stock` : title}
     </button>
   );
